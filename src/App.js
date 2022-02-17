d1d5d635dea2975a7f0384dde646d7e70fb6f4d3
Abhishek Kumar <68541838+19abhishek@users.noreply.github.com>
2022-02-17 10:56:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+const list = ["Ahmedabad", "Banglore", "Delhi", "Mumbai", "Chennai", "Dhanbad"];
+
+export default function App() {
+  const [cities, setCities] = useState(list);
+  const [val, setVal] = useState("");
+
+  const handleInput = (e) => {
+    setVal(e.target.value);
+  };
+
+  useEffect(() => {
+    setCities(list.filter((city) => city.includes(val)));
+  }, [val]);
+
+  return (
+    <div className="App">
+      <input type="text" value={val} onChange={handleInput} />
+      <ul>
+        {cities.map((city, idx) => {
+          return <li key={idx}>{city}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
